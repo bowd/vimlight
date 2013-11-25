355b1d50bf7ecfe80255cea451abc5dbdd313dad
Bogdan Radu Dumitru <dumbogdan@gmail.com>
2013-11-25 22:19:19 +0200

    Make installer prettier

--- bin/install.sh
+++ bin/install.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
+set -e
+
 BACKUP_DIR=~/.vim.bak
 REPO="git@github.com:bogdan-dumitru/vimlight.git"
+
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+RESET="\e[39m"
+INFO="[$GREEN""info""$RESET]"
+ERROR="[$RED""error""$RESET]"
+
 
 function backup_config {
 	# Check if there's already a backup
 	if [ -e "$BACKUP_DIR" ]; then
-		echo "$BACKUP_DIR exists. Please manually rotate / remove the current backup and try again."
+		echo -e "$ERROR $BACKUP_DIR exists. Please manually rotate / remove the current backup and try again."
 		exit
 	fi
 
 	# Create the backup dir
+	echo -e "$INFO Backing up current vim configuration to $GREEN $BACKUP_DIR $RESET"
 	mkdir "$BACKUP_DIR"
 	# Move all vim related folders to backup
 	for entry in `ls -d ~/.vim* | grep -v $BACKUP_DIR`; do
-		echo "Moving $entry to $BACKUP_DIR..."
+		echo -e "$INFO     $entry"
 		mv "$entry" $BACKUP_DIR
 	done
 }
 
 function setup_config {
 	# Clone repo into ~/.vim
+	echo ""
+	echo -e "$INFO Downloading vimlight from github..."
 	git clone $REPO  ~/.vim
 
 	# Link .vimrc file
+	echo ""
+	echo -e "$INFO Linking .vimrc to ~"
 	ln -s ~/.vim/.vimrc ~/.vimrc
 
 	# Create .vimrc.local from example?
+	echo ""
+	echo -e "$INFO Creating ~/.vimrc.local"
 	finish="-1"
 	while [ "$finish" = '-1' ]; do
 		finish="1"
@@ -38,18 +55,24 @@ function setup_config {
 		esac
 	done
 
+	echo ""
+	echo -e "$INFO Creating ~/.vimrc.bundles.local"
 	# Create an empty bundle.local
-	touch ~/.vimrc.bundle.local
+	touch ~/.vimrc.bundles.local
 
 	# Setup Vundle
+	echo ""
+	echo -e "$INFO Downloading the vundle plugin from github..."
 	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 	# Install bundles
+	echo ""
+	echo -e "$INFO Installing bundles"
 	vim +BundleInstall +qall
 
-	echo "Setup complete."
-	echo "Add your personal vim config to ~/.vimrc.local"
-	echo "Add additional bundles to ~/.vimrc.bundle.local"
+	echo -e "$GREEN""Setup complete.""$RESET"
+	echo -e "$YELLOW""Add your personal vim config to ~/.vimrc.local""$RESET"
+	echo -e "$YELLOW""Add additional bundles to ~/.vimrc.bundle.local""$RESET"
 }
 
 function main {
